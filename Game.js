bd579683a6d00b4b57df13872c8553054cd18ab3
gowri666 <71494930+gowri666@users.noreply.github.com>
2020-11-16 17:04:02 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,59 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+ async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef= await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount= playerCountRef.val();
+        player.getCount();
+      }
+     
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hide();
+    textSize(30);
+    text("game started",120,100)
+    Player.getPlayerInfo();
+    if(allPlayers!== undefined){
+      var pos= 130;
+      for(var plr in allPlayers){
+        if(plr==="player"+ player.index){
+          fill("red");
+
+        } 
+        else {
+
+          fill("black");
+
+        }
+        pos = pos+ 20
+        textSize(15)
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,pos);
+
+      }
+    }
+    if(keyDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50;
+      player.update();
+    }
+  }
+}
